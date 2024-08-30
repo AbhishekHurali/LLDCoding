56ccba6b29f00a966526a619d424c8382a9ebbb4
AbhishekHurali <abhishekhurali19@gmail.com>
2024-08-30 16:05:40 +0530

    JDBC

--- JDBC/Client.java
+++ JDBC/Client.java
@@ -9,20 +9,33 @@ public class Client {
     public static void main(String[] args) throws SQLException {
         Connection con = null;
         Statement st = null;
-        //Open the Connection
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/abhidb","root","scaler#abhi19@mysql");
-        //System.out.println(con);
-        //Prepare SQL Statement
-        String SQL = "insert into stud values(1921,'AbhiJaya',100)";
-        //Create JDBC statement
-        st = con.createStatement();
-        //Submit SQL to DB
-        int x = st.executeUpdate(SQL);
-        //Processing the result
-        if(x==1){
-            System.out.println("Customer Record is inserted successfully");
-        }else{
-            System.out.println("Sorry, Customer Record is not Inserted");
+        try{
+
+            //Open the Connection
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/abhidb","root","scaler#abhi19@mysql");
+            //System.out.println(con);
+            //Prepare SQL Statement
+            String SQL = "insert into stud values(1921,'AbhiJaya',100)";
+            //Create JDBC statement
+            st = con.createStatement();
+            //Submit SQL to DB
+            int x = st.executeUpdate(SQL);
+            //Processing the result
+            if(x==1){
+                System.out.println("Customer Record is inserted successfully");
+            }else{
+                System.out.println("Sorry, Customer Record is not Inserted");
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            if(st!=null){
+                st.close();
+            }
+            if(con!=null){
+                con.close();
+            }
         }
     }
 }
